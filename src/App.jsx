4edaf481584abd9a10870c072e71257408014682
Ashish Srivastava <ashishsrivastava@Ashishs-MacBook-Pro.local>
2023-03-10 21:04:00 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "../assets/tesla.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo w-96" alt="logo" />
+        <nav>
+          <h5>Model S</h5>
+          <h5>Model 3</h5>
+          <h5>Model X</h5>
+          <h5>Model Y</h5>
+          
+        </nav>
+        <div>
+            <h5>Solar Roof</h5>
+          <h5>Solar Panels</h5>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
